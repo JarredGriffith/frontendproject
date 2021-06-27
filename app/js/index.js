1b5ac956bae65aebf6eceb9419eea6562ec78b7b
jarre <jarredgriffith29@gmail.com>
2021-06-26 17:01:53 -0700

    updating

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,20 @@
-console.log("test")
+
+const btnhambuger = document.querySelector('#btnhambuger') //grabs the ham buger menu.
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+
+    btnhambuger.addEventListener('click',function(){ //closes
+        //function checks to see if the element has class.
+        if(header.classList.contains('open')){
+            //if it has the class open then it removes it.
+            header.classList.remove('open')
+            overlay.classList.remove('fade-in')
+            overlay.classList.add('fade-out')
+        }
+        else {
+            //else adds open class. 
+            header.classList.add('open') // opens menu 
+            overlay.classList.remove('fade-out')
+            overlay.classList.add('fade-in')
+        }
+    })
